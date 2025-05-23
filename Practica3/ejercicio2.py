22bb82730b2dda22bb14d06cb639b9490cef7d93
WangXing117 <123046757@upq.edu.mx>
2025-05-22 20:05:25 -0600

    Ejercicio 3
    Descubrir si hay un palindromo o no

--- Practica3/ejercicio2.py
+++ Practica3/ejercicio2.py
@@ -2,14 +2,17 @@ repetir = True
 while repetir:
     try:
         anio = int(input("Ingresa un año, ejemplo 2025: "))
-        if (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0):
-            print(f"{anio} es un año bisiesto.\n")
+        if anio<0 or anio==0:
+            print("Ingresa valores positivos")
         else:
-            print(f"{anio} no es un año bisiesto.\n")  
+            if (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0):
+                print(f"{anio} es un año bisiesto.\n")
+            else:
+                print(f"{anio} no es un año bisiesto.\n")  
     except ValueError:
         print("Por favor, ingresa un número válido para el año.")
     
-    match input("Ingresa 's' si quieres ingresar otra fecha, si no ingres 'n': "):
+    match input("Ingresa 's' si quieres ingresar otra fecha, si no ingresa 'n': "):
         case 's':
             repetir=True
         case _:
---
+++ Practica3/ejercicio3.py
@@ -0,0 +1,18 @@
+repetir = True
+while repetir:
+    cadena = input("Ingresa una cadena de texto: ").replace(" ", "").lower()
+    if cadena.isalpha()!=True:
+        print("Solo palabras")
+    elif cadena == "":
+        print("La cadena esta vacia")
+    else:
+        if cadena == cadena[::-1]:
+            print("La cadena es palíndroma.\n")
+        else:
+            print("La cadena no es palíndroma.\n")
+    
+    match input("Ingresa 's' si quieres ingresar otra fecha, si no ingresa 'n': "):
+        case 's':
+            repetir=True
+        case _:
+            repetir=False
