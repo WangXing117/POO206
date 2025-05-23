5136cb4aba4968a6b56acb05e439c19a08b3250d
WangXing117 <123046757@upq.edu.mx>
2025-05-22 19:30:54 -0600

    Ejercicio 1
    actividad de pedir un número y saber si es par o no

---
+++ Practica3/ejercicio1.py
@@ -0,0 +1,23 @@
+#1.pide al usuario un número entero y muestra por pantalla si es par o no
+repetir = True
+
+while True:
+    numero = 0
+    repetir = True
+    try:
+        numero = int(input("Ingresa un número entero: "))
+    except ValueError:
+         print("Ingrese un número entero por favor \n")
+         repetir=False
+         
+    if repetir==True:
+        if numero % 2 == 0:
+            print(f"El número {numero} es par")
+        else:
+            print(f"El número {numero} es impar")
+            
+    opcion = input("Ingres 's' si quires ingresar otro número y 'n' si ya no: ")
+    if opcion!="s":
+        break
+        
+        
