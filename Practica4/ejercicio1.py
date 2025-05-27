496e48a253b77182ce36a3080034aa32e3fb3cc5
WangXing117 <123046757@upq.edu.mx>
2025-05-27 17:45:12 -0600

    ejercicio 1 practica 4
    Ejercicio que lee cualquier numero arriba de 10 y te dice los impares entre 2 y el número

---
+++ Practica4/ejercicio1.py
@@ -0,0 +1,22 @@
+# 1. Escribir un programa que pida al usuario un número entero positivo mayor de 10 y
+# que muestre como resultado todos los números impares desde 2 hasta ese número
+# separados por comas.
+repetir = True
+while repetir:
+    try:
+        numero = int(input("Ingresa un número entero mayor que 10: "))
+        if numero <=10:
+            print("EL número debe de ser entero mayor a 10")
+        else:
+            numerosImpares = [str(numeroImpar) for numeroImpar in range(2,numero+1) if numeroImpar % 2 != 0]
+            print(", ".join(numerosImpares))
+            
+    except ValueError:
+         print("Ingrese un número entero por favor \n")
+         repetir=False
+
+    match input("Ingresa 's' si quieres ingresar otra fecha, si no ingresa 'n': "):
+        case 's':
+            repetir=True
+        case _:
+            repetir=False
