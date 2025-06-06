2926c18cfa18f39f3e06edd41ba4f00177c454b1
WangXing117 <123046757@upq.edu.mx>
2025-06-05 19:53:43 -0600

    Ejercicio 2
    Se agrega el ejercicio 2 sobre pares entre los número 200 y 400

---
+++ ExamenParcial1/ejercicio2.py
@@ -0,0 +1,24 @@
+# Crea un programa que solicite un número par commprendido entre 200 y 400
+# y nos muestre todos los números paren en la serie entre el número y 400
+
+
+repetir = True
+
+while repetir:
+    try:
+        numeroBase = int( input("Ingresa un número entre 200 y 400:" ))
+        if numeroBase <200 or numeroBase > 400:
+            print(f"Error: {numeroBase} no esta entre el rango de 200 y 400")
+        else:
+            listaNum = [i for i in range(numeroBase,401) if i%2 == 0]
+            print(listaNum)
+    except (ValueError):
+        print("Se tiene que ingresar un número")
+    except:
+        print("Ha habido un error reintente por favor")
+        
+    match input("Ingresa 's' si quieres reintentar si no cualquier otra cosa: "):
+        case 's':
+            repetir = True
+        case _:
+            repetir = False
