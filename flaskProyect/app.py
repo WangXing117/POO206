ebb3d99bc79652ed2694554eed40ce59d56668cd
WangXing117 <123046757@upq.edu.mx>
2025-06-11 15:24:56 -0600

    Termino de hola mundo
    Se crea la ruta para que la página home diga "Hola mundo FLASK"

--- flaskProyect/app.py
+++ flaskProyect/app.py
@@ -3,5 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def home(): #para que inicie la interfaz home
+    return 'Hola mundo FLASK'
+
 if __name__ == '__main__':
     app .run(port = 3000, debug = True)
+    
